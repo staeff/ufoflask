c7f531dfae598bbffe2d4db5ca4a7a4857127462
Stephan Klinger <staeff@arcor.de>
2015-02-18 16:35:00 +0000

    Initialize db

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Path of the database file for Flask-SQLAlchemy
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+print SQLALCHEMY_DATABASE_URI
---
+++ init-db.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from routes import db
+
+db.create_all()
