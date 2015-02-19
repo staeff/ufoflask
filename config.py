1b42f1d5f558dcf3c083dfbb2b1e3c62e189fa51
Stephan Klinger <staeff@arcor.de>
2015-02-19 17:36:05 +0000

    Take secrets from env-vars

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+DBUSER = os.environ.get("DBUSER", "")
+PW = os.environ.get("PW", "")
 
 # Path of the database file for Flask-SQLAlchemy
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# DBUSER={user} PW={password} python routes.py
+SQLALCHEMY_DATABASE_URI = 'mysql://{0}:{1}@localhost/ufosightings'.format(DBUSER,PW)
