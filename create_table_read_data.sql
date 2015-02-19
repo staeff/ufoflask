2d468ff432c3b5a6056ea7961f35fec7a213aa44
Stephan Klinger <staeff@arcor.de>
2015-02-19 16:19:08 +0000

    Add create table and read data script

---
+++ create_table_read_data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS sightings (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sighted_at INT(8),
+    reported_at INT(8),
+    location VARCHAR(100),
+    shape VARCHAR(10),
+    duration VARCHAR(10),
+    description TEXT,
+    lat FLOAT(10, 6),
+    lng FLOAT(10, 6)
+  ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
+LOAD DATA LOCAL INFILE 'sightings.tsv' INTO TABLE sightings FIELDS TERMINATED BY '\t' ENCLOSED BY '"' LINES TERMINATED BY '\n';
